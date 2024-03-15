ef770a717f1c0b7c55216d023999eda4aeb6b4f1
Jitender-rana <119347955+Jitender-rana@users.noreply.github.com>
2024-03-15 23:16:57 +0530

    Added reverse_array

---
+++ try11.java
@@ -0,0 +1,22 @@
+public class try11 {
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5,6};
+        int size=arr.length;
+
+        int i=0;
+        while(i<size/2){
+            int temp;
+            temp=arr[i];
+            arr[i]=arr[size-i-1];
+            arr[size-i-1]=temp;
+            i++;
+
+
+        }
+        for(int j=0;j<arr.length;j++){
+            System.out.print(arr[j]+" ");
+
+        }
+    }
+    
+}
