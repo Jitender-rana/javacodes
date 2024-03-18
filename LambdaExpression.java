212da9985cd99d35054a6c1783b595843a550035
Jitender-rana <119347955+Jitender-rana@users.noreply.github.com>
2024-03-18 11:43:31 +0530

    Update LambdaExpression.java

--- LambdaExpression.java
+++ LambdaExpression.java
@@ -11,12 +11,17 @@ public class LambdaExpression {
     public static void main(String args[]){
         int width=8;
         //without lambda ,Drawable implementation using anonymous class---(arg list)->{body}
-         Drawable d=new Drawable(){
-            public void draw(){
-                System.out.println("Java Version is: "+width);
-             }
+        //  Drawable d=new Drawable(){
+        //     public void draw(){
+        //         System.out.println("Java Version is: "+width);
+        //      }
 
-         };
+        //  };
+        //or Another way to do same thing is by using lambda expression with no arguments.
+        Drawable d=()->{
+            System.out.println("Java Version is: "+width);
+
+        };
         d.draw();
     }
     
