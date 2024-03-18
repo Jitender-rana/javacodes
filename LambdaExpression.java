a90f02ab161a4af6f2b65e707465a9117444bb48
Jitender-rana <119347955+Jitender-rana@users.noreply.github.com>
2024-03-18 11:35:19 +0530

    Added LambdaExpression.java

---
+++ LambdaExpression.java
@@ -0,0 +1,23 @@
+public class LambdaExpression {
+    /**
+     * InnerLambdaExpression
+     */
+    interface Drawable {
+        public void draw();
+
+    
+        
+    }
+    public static void main(String args[]){
+        int width=8;
+        //without lambda ,Drawable implementation using anonymous class
+         Drawable d=new Drawable(){
+            public void draw(){
+                System.out.println("Java Version is: "+width);
+             }
+
+         };
+        d.draw();
+    }
+    
+}
