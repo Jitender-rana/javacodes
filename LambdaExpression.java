4919ddbb2b2bf155c176d3673250b23e7211c094
Jitender-rana <119347955+Jitender-rana@users.noreply.github.com>
2024-03-18 11:37:06 +0530

    Update LambdaExpression.java

--- LambdaExpression.java
+++ LambdaExpression.java
@@ -10,7 +10,7 @@ public class LambdaExpression {
     }
     public static void main(String args[]){
         int width=8;
-        //without lambda ,Drawable implementation using anonymous class
+        //without lambda ,Drawable implementation using anonymous class---(arg list)->{body}
          Drawable d=new Drawable(){
             public void draw(){
                 System.out.println("Java Version is: "+width);
